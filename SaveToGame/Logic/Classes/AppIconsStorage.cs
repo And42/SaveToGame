98c3831f9eed23d4d618868cb4ba8dd762f801a8
And42 <andrewskobcov@yandex.ru>
2018-07-28 18:35:11 +0300

    Refactoring

--- SaveToGame/Logic/Classes/AppIconsStorage.cs
+++ SaveToGame/Logic/Classes/AppIconsStorage.cs
@@ -1,27 +1,17 @@
 ﻿using System.Windows.Media.Imaging;
-using MVVM_Tools.Code.Classes;
 using MVVM_Tools.Code.Providers;
 using SaveToGameWpf.Logic.Utils;
 
 // ReSharper disable InconsistentNaming
-// ReSharper disable UnusedAutoPropertyAccessor.Local
 
 namespace SaveToGameWpf.Logic.Classes
 {
-    public class AppIconsStorage : BindableBase
+    public class AppIconsStorage
     {
-        public Property<BitmapSource> Icon_xxhdpi { get; private set; }
-        public Property<BitmapSource> Icon_xhdpi { get; private set; }
-        public Property<BitmapSource> Icon_hdpi { get; private set; }
-        public Property<BitmapSource> Icon_mdpi { get; private set; }
-
-        public AppIconsStorage()
-        {
-            BindProperty(() => Icon_xxhdpi);
-            BindProperty(() => Icon_xhdpi);
-            BindProperty(() => Icon_hdpi);
-            BindProperty(() => Icon_mdpi);
-        }
+        public Property<BitmapSource> Icon_xxhdpi { get; } = new Property<BitmapSource>();
+        public Property<BitmapSource> Icon_xhdpi { get; } = new Property<BitmapSource>();
+        public Property<BitmapSource> Icon_hdpi { get; } = new Property<BitmapSource>();
+        public Property<BitmapSource> Icon_mdpi { get; } = new Property<BitmapSource>();
 
         public byte[] GetXxhdpiBytes() => GetBytes(Icon_xxhdpi);
         public byte[] GetXhdpiBytes() => GetBytes(Icon_xhdpi);
